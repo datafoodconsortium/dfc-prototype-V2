84cb8c45ab4fecb44107205125be164ab03084b6
simon louvet <simon.louvet.zen@gmail.com>
2019-05-02 10:59:55 +0200

    consume json-ld

--- src/model/catalog.js
+++ src/model/catalog.js
@@ -1,5 +1,6 @@
 import Navigo from 'navigo';
 import GenericElement from '../core/genericElement.js';
+import config from '../config.json';
 export default class Catalog extends GenericElement {
   constructor() {
     super();
@@ -13,20 +14,31 @@ export default class Catalog extends GenericElement {
 
   }
 
-  loadAll(){
-    let data=[
-      {
-        name : 'product1'
-      },
-      {
-        name : 'product2'
-      }
-    ]
-    this.publish({
-      channel: 'catalog',
-      topic: 'changeAll',
-      data: data
-    });
+  loadAll() {
+    console.log(config);
+    config.sources.forEach(source => {
+      fetch(source).then(response => {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            throw new Error(response.status)
+          } else {
+            return response.json();
+          }
+        }).then(data => {
+          console.log(data);
+          let out = data['DFC:Entreprise']['DFC:manages'].map(manage => manage['DFC:references']);
+          console.log(out);
+          this.publish({
+            channel: 'catalog',
+            topic: 'changeAll',
+            data: out
+          });
+        })
+        .catch(function(err) {
+          console.log('Fetch Error :', err);
+        });
+    })
   }
 }
 window.customElements.define('x-service-catalog', Catalog);
